3e9eb7f0cb8e003cdad050c6f05abccdfbf254fb
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-02-14 15:29:27 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -3,23 +3,56 @@
 #include <cstdlib>
 
 const int NUM = 10;
+const int ROWS = 10;
+const int COLUMNS = 5;
 
 void get_names(std::string *names);
 //Precondition: The array for the names must have 10 indices
 //Postcondtion: Stores the names within the 'names' text file within an array
 
+void get_scores(int *scores);
+
 int main()
 {
     std::string names[NUM];
 
-    get_names(names);
+    int scores[ROWS][COLUMNS];
 
+    //get_names(names);
+
+    /*
     for (int i = 0; i < 10; i++)
     {
         std::cout << names[i] << std::endl;
     }
+    */
 
     return 0;
+}
+
+void get_scores(int *scores)
+{
+    char next;
+    std::ifstream ins;
+    int row = 0, column = 0;
+
+
+    ins.open("test scores.txt");
+    if (ins.fail())
+    {
+        std::cout << "The file failed to open" << std::endl;
+        exit(1);
+    } 
+
+    while(!ins.eof())
+    {
+        if (next == " ")
+        {
+
+        }
+
+        ins.get(next);
+    }
 }
 
 void get_names(std::string *names)
@@ -30,7 +63,7 @@ void get_names(std::string *names)
     int index = 0;
 
     ins.open("names.txt");
-    if(ins.fail())
+    if (ins.fail())
     {
         std::cout << "The file failed to open" << std::endl;
         exit(1);
@@ -47,4 +80,6 @@ void get_names(std::string *names)
         ins.get(next);
         name += next;
     }
+
+    ins.close();
 }
