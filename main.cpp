9f8abdac7ae7f5ee671dbc5ea3357f7a0a73cfd7
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-02-14 15:53:19 +0200

    There is a bug in the get_scores algo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 const int NUM = 10;
 const int ROWS = 10;
@@ -10,7 +11,9 @@ void get_names(std::string *names);
 //Precondition: The array for the names must have 10 indices
 //Postcondtion: Stores the names within the 'names' text file within an array
 
-void get_scores(int *scores);
+void get_scores(int scores[ROWS][COLUMNS]);
+//Precondition: The 2-dimensional array must be empty
+//Postcondition: Stores the scores with test scores file in a 2-dimensional array
 
 int main()
 {
@@ -18,6 +21,7 @@ int main()
 
     int scores[ROWS][COLUMNS];
 
+    get_scores(scores);
     //get_names(names);
 
     /*
@@ -30,29 +34,40 @@ int main()
     return 0;
 }
 
-void get_scores(int *scores)
+void get_scores(int scores[ROWS][COLUMNS])
 {
     char next;
+    std::string number ="";
     std::ifstream ins;
     int row = 0, column = 0;
-
+    int score;
 
     ins.open("test scores.txt");
     if (ins.fail())
     {
         std::cout << "The file failed to open" << std::endl;
         exit(1);
-    } 
+    }
 
     while(!ins.eof())
     {
-        if (next == " ")
+        if (next == ' ')
         {
-
+            score = std::stoi(number);
+            std::cout << score << std::endl;
+            scores[row][column] = score;
+            column = column + 1;
+            number = "";
+        } else if (next == '\n') {
+            row = row + 1;
         }
 
         ins.get(next);
+        number += next;
+
     }
+
+    ins.close();
 }
 
 void get_names(std::string *names)
