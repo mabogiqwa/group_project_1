8c2d61605962fc0aff8d07b8bbe21b4dabfb52a7
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-02-20 18:06:47 +0200

    Fixed get_scores error and updated calculate averages function

--- main.cpp
+++ main.cpp
@@ -15,20 +15,47 @@ void get_scores(int scores[ROWS][COLUMNS]);
 //Precondition: The 2-dimensional array must be empty
 //Postcondition: Stores the test scores with the scores file into a 2-dimensional array
 
+void calculate_average(int scores[ROWS][COLUMNS], double *averages);
+//Precondition: The scores 2-d array must be populated with values
+//Postcondition: The function must populate a 1-d array with the averages of each student
+
 int main()
 {
     std::string names[NUM];
 
     int scores[ROWS][COLUMNS];
 
-    get_scores(scores);
+    double averages[ROWS];
 
-    //Shivam you can write the code that calculate the average of 
-    //each of the students test score here
-    //Note that each row of the 2-dimensional array has the test scores of each student
-    //So while traversing through a row, you can sum up the scores and divide them by the number of columns to get the average of the student.
+    get_names(names);
+    get_scores(scores);
+    calculate_average(scores, averages);
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        std::cout << averages[i] << " ";
+    }
 
     return 0;
+}
+
+void calculate_average(int scores[ROWS][COLUMNS], double *averages)
+{
+    int rowSum;
+    int studentAverage;
+    int index = 0;
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        rowSum = 0;
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            rowSum += scores[i][j];
+        }
+        studentAverage = double(rowSum)/COLUMNS;
+        averages[index] = studentAverage;
+        index++;
+    }
 }
 
 void get_scores(int scores[ROWS][COLUMNS])
@@ -48,6 +75,37 @@ void get_scores(int scores[ROWS][COLUMNS])
 
     while (ins.get(next))
     {
+        if (next == ' ' || next == '\n')
+        {
+            if (!number.empty())
+            {
+                score = std::stoi(number);
+                scores[row][column] = score;
+                number = "";
+                column++;
+
+                if (next == '\n') // Move to next row
+                {
+                    row++;
+                    column = 0;
+                }
+            }
+        }
+        else
+        {
+            number += next;
+        }
+    }
+
+    if (!number.empty())
+    {
+        score = std::stoi(number);
+        scores[row][column] = score;
+    }
+
+    /*
+    while (ins.get(next))
+    {
         if (next == ' ')
         {
             score = std::stoi(number);
@@ -65,7 +123,6 @@ void get_scores(int scores[ROWS][COLUMNS])
         } else {
             number += next;
         }
-
     }
 
     if (!number.empty())
@@ -74,6 +131,7 @@ void get_scores(int scores[ROWS][COLUMNS])
         scores[row][column] = score;
         std::cout << score << std::endl;
     }
+    */
 
     ins.close();
 }
